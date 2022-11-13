569854ef34032a501fc91c3934d544f848d2e810
DevinLlerena23 <Devinllerena12@gmail.com>
2022-11-13 10:02:04 -0500

    Agregar archivos de proyecto.

---
+++ GitLlerenaDevin/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitLlerenaDevin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nombre: Devin Fabricio");
+            Console.WriteLine("Apellidos: Llerena Ballesteros");
+            Console.WriteLine("Curso: 2 do de Tics");
+            Console.WriteLine("Asignatura: Programacion estructurada y funcional ");
+            Console.WriteLine("Fecha: 13/11/2022");
+            Console.WriteLine("Tema: Operadores logicos");
+            
+
+            Console.ReadKey();
+        }
+    }
+}

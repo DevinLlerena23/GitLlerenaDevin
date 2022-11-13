1e0ff31dcd6ca503581fde32bc6e34260844ccae
DevinLlerena23 <Devinllerena12@gmail.com>
2022-11-13 10:26:01 -0500

    Dia aleatorio

--- GitLlerenaDevin/Program.cs
+++ GitLlerenaDevin/Program.cs
@@ -16,7 +16,47 @@ namespace GitLlerenaDevin
             Console.WriteLine("Asignatura: Programacion estructurada y funcional ");
             Console.WriteLine("Fecha: 13/11/2022");
             Console.WriteLine("Tema: Operadores logicos");
-            
+
+            var Random = new Random().Next(1, 8);
+
+            if(Random==1)
+            {
+                Console.WriteLine("Lunes ");
+            }
+            else if(Random==2)
+            {
+                Console.WriteLine("Martes");
+            }
+            else if (Random == 3)
+            {
+                Console.WriteLine("Miercoles");
+
+            }
+            else if (Random == 4)
+            {
+                Console.WriteLine("Jueves");
+
+            }
+            else if (Random == 5)
+            {
+                Console.WriteLine("Viernes");
+
+            }
+            else if (Random == 6)
+            {
+                Console.WriteLine("Sabado");
+
+            }
+            else if (Random == 7)
+            {
+                Console.WriteLine("Domingo");
+
+            }
+            else if (Random == 2)
+            {
+                Console.WriteLine("Martes");
+
+            }
 
             Console.ReadKey();
         }

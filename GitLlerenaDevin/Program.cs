085382585c74a599148fcefbbaea8e5a0837bd1f
DevinLlerena23 <Devinllerena12@gmail.com>
2022-11-13 10:57:55 -0500

    cambio en los numeros random

--- GitLlerenaDevin/Program.cs
+++ GitLlerenaDevin/Program.cs
@@ -19,70 +19,74 @@ namespace GitLlerenaDevin
 
             var Random = new Random().Next(1, 8);
 
-            if(Random==1)
+            Console.WriteLine("El numero es {0}", Random);
+            if (Random==1)
             {
-                Console.WriteLine("El numero es {0}",Random);
+               
 
                 Console.WriteLine("Lunes ");
             }
             else if(Random==2)
             {
-                Console.WriteLine("El numero es {0}", Random);
+               
 
                 Console.WriteLine("Martes");
             }
             else if (Random == 3)
             {
-                Console.WriteLine("El numero es {0}", Random);
+              
 
                 Console.WriteLine("Miercoles");
 
             }
             else if (Random == 4)
             {
-                Console.WriteLine("El numero es {0}", Random);
+                
 
                 Console.WriteLine("Jueves");
 
             }
             else if (Random == 5)
             {
-                Console.WriteLine("El numero es {0}", Random);
+               
 
                 Console.WriteLine("Viernes");
 
             }
             else if (Random == 6)
             {
-                Console.WriteLine("El numero es {0}", Random);
+                
 
                 Console.WriteLine("Sabado");
 
             }
             else if (Random == 7)
             {
-                Console.WriteLine("El numero es {0}", Random);
+              
 
                 Console.WriteLine("Domingo");
 
             }
             else if (Random == 2)
             {
-                Console.WriteLine("El numero es {0}", Random);
+               
 
                 Console.WriteLine("Martes");
 
             }
 
             var r = new Random().Next(1, 101);
-            if(!(r>=18 && r<=50))
+
+            Console.WriteLine("El numero es {0}", r);
+
+            if (!(r>=18 && r<=50))
             {
-                Console.WriteLine("El numero es {0}", r);
+               
                 Console.WriteLine("Esta poblacion es Trabajadora");
             }
             else
             {
-                Console.WriteLine("El numero es {0}", r);
+                
                 Console.WriteLine("Esta poblacion No es economicamente activa");
             }
             Console.ReadKey();
